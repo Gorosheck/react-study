6d255d059a3db76877c54d59f8c6fc8390d6d2b4
Kirill Goroshko <gorosheck@gmail.com>
2023-03-17 19:19:20 +0300

    lesson 7 done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ import { render } from '@testing-library/react';
 const App = () => {
 
   return (
-    <div className="App">
-      Simple HTMl
+    <div>
+      <div><Header /></div>
+      <div><Tech /></div>
     </div>
   );
+}
 
+const Header = () => {
+  return (<div>
+    <a href="#">home</a>
+    <a href="#">news</a>
+    <a href="#">contact</a>
+    <a href="#">other</a>
+  </div>
+  );
+}
+
+const Tech = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
