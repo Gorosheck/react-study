5ebc10baaf523db3365233a467f7450a67aea2fe
Kirill Goroshko <gorosheck@gmail.com>
2023-03-18 20:43:01 +0300

    lesson 9 done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
+import Header from './Header';
+import Technologies from './Technologies';
+
 
 const App = () => {
 
   return (
     <div>
       <div><Header /></div>
-      <div><Tech /></div>
+      <div><Technologies /></div>
     </div>
   );
 }
 
-const Header = () => {
-  return (<div>
-    <a href="#">home</a>
-    <a href="#">news</a>
-    <a href="#">contact</a>
-    <a href="#">other</a>
-  </div>
-  );
-}
 
-const Tech = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  );
-}
+
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+   return (<div>
+      <a href="#">home</a>
+      <a href="#">news</a>
+      <a href="#">contact</a>
+      <a href="#">other</a>
+   </div>
+   );
+}
+
+export default Header;
