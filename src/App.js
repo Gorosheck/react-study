0889e3851062ec13e08cc69e765f2cc9e0dd8b29
Kirill Goroshko <gorosheck@gmail.com>
2023-03-25 19:36:38 +0300

    lesson 19 homework complete

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 
 
 
@@ -19,6 +22,9 @@ const App = () => {
           <Routes>
             <Route path='/dialogs' element={<Dialogs />} />
             <Route path='/profile' element={<Profile />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
