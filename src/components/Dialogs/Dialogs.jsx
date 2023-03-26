a565d04bca7be9a806c62cac6774768d373c35a8
Kirill Goroshko <gorosheck@gmail.com>
2023-03-26 12:50:28 +0300

    lesson 22 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
    return (
       <div className={classes.dialogs}>
          <div className={classes.dialogsItems}>
-            <div className={classes.dialog + ' ' + classes.active}>Леша</div>
-            <div className={classes.dialog}>Артем</div>
-            <div className={classes.dialog}>Андрей</div>
-            <div className={classes.dialog}>Лагуна</div>
-            <div className={classes.dialog}>Дима</div>
+            <div className={classes.dialog + ' ' + classes.active}>
+               <NavLink to='/dialogs/1'>Леша</NavLink>
+            </div>
+            <div className={classes.dialog}>
+               <NavLink to='/dialogs/2'>Артем</NavLink>
+            </div>
+            <div className={classes.dialog}>
+               <NavLink to='/dialogs/3'>Дима</NavLink>
+            </div>
+            <div className={classes.dialog}>
+               <NavLink to='/dialogs/4'>Лагуна</NavLink>
+            </div>
+            <div className={classes.dialog}>
+               <NavLink to='/dialogs/5'>Андрей</NavLink>
+            </div>
          </div>
          <div className={classes.messages}>
             <div className={classes.message}>Hi</div>
