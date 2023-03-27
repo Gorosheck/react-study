43e6014055dc5ed9891e5b1a9ef61033995d32eb
Kirill Goroshko <gorosheck@gmail.com>
2023-03-27 11:46:02 +0300

    Lesson 26 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
+import Message from './Message/Message';
+import DialogItem from './DialogItem/DialogItem';
 
-const DialogItem = (props) => {
-   let path = '/dialogs/' + props.id;
-   return (
-      <div className={classes.dialog + ' ' + classes.active}>
-         <NavLink to={path}>{props.name}</NavLink>
-      </div>
-
-   );
-}
-
-const Message = (props) => {
-   return (
-      <div className={classes.message}>{props.message}</div>
-   );
-}
 
 const Dialogs = (props) => {
 
