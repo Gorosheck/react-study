40a79a7b9b8572974b6b80f56a94ad171dadd010
Kirill Goroshko <gorosheck@gmail.com>
2023-04-01 18:02:47 +0300

    lesson 35 done (use observer)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { addPost, updateNewPostText } from './redux/state';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export let rerenderEntireTree = (state) => {
+   root.render(
+      <React.StrictMode>
+         <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      </React.StrictMode>
+   );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 
 // If you want to start measuring performance in your app, pass a function
