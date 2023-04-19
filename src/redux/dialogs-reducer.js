deaa41e3c6c11d589eee93728a552355cec7c73f
Kirill Goroshko <gorosheck@gmail.com>
2023-04-19 20:41:55 +0300

    install Redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,25 @@
 const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+   messages: [
+      { id: 1, message: 'Hi' },
+      { id: 2, message: 'How are you' },
+      { id: 3, message: 'Yo' },
+      { id: 4, message: 'Yo' },
+      { id: 5, message: 'Yo' }
+   ],
+   dialogs: [
+      { id: 1, name: 'Леша' },
+      { id: 2, name: 'Артем' },
+      { id: 3, name: 'Дима' },
+      { id: 4, name: 'Лагуна' },
+      { id: 5, name: 'Андрей' }
+   ],
+   newMessageBody: ""
+}
+
+const dialogsReducer = (state = initialState, action) => {
 
    switch (action.type) {
       case UPDATE_NEW_MESSAGE_BODY:
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,17 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+   postsData: [
+      { id: 1, message: 'How are you?', likesCount: 12 },
+      { id: 2, message: 'Its my first post', likesCount: 11 },
+      { id: 3, message: 'Its my first post', likesCount: 11 },
+      { id: 4, message: 'Its my first post', likesCount: 11 }
+   ],
+   newPostText: "fix value text for post"
+}
+
+const profileReducer = (state = initialState, action) => {
 
    switch (action.type) {
       case ADD_POST:
