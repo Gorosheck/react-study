2778efb97ef8f93093073e125ff9211b7c463763
Kirill Goroshko <gorosheck@gmail.com>
2023-04-29 19:45:22 +0300

    lesson 45 done (refactoring MyPostsContainer

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-export let rerenderEntireTree = (state) => {
+export let rerenderEntireTree = () => {
    root.render(
       <React.StrictMode>
          <Provider store={store}>
