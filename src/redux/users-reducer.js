f82f073fb392fc137ae195fe0ee2ad0a6f5790b3
Kirill Goroshko <gorosheck@gmail.com>
2023-05-04 21:45:51 +0300

    create follow-unfollow

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,35 +1,34 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
 
 let initialState = {
    users: [
-      { id: 1, fullName: 'Sasha', status: '12', location: { city: 'Minsk', country: 'Belarus' } },
-      { id: 2, fullName: 'Andrew', status: '11', location: { city: 'Moscow', country: 'Belarus' } },
-      { id: 3, fullName: 'Alex', status: '11', location: { city: 'Kiev', country: 'Belarus' } },
-      { id: 4, fullName: 'Vova', status: '11', location: { city: 'NY', country: 'Belarus' } }
+      { id: 1, followed: false, fullName: 'Sasha', status: '12', location: { city: 'Minsk', country: 'Belarus' } },
+      { id: 2, followed: true, fullName: 'Andrew', status: '11', location: { city: 'Moscow', country: 'Belarus' } },
+      { id: 3, followed: true, fullName: 'Alex', status: '11', location: { city: 'Kiev', country: 'Belarus' } },
+      { id: 4, followed: false, fullName: 'Vova', status: '11', location: { city: 'NY', country: 'Belarus' } }
    ]
 }
 
 const usersReducer = (state = initialState, action) => {
 
    switch (action.type) {
+      case FOLLOW:
 
+
+      case UNFOLLOW:
       default:
          return state;
    }
 }
 
-export const addPostActionCreator = () => {
+export const followAC = (userId) => {
    return {
-      type: ADD_POST
+      type: FOLLOW,
+      userId
    }
 }
 
-export const updateNewPostTextActionCreator = (text) => {
-   return {
-      type: UPDATE_NEW_POST_TEXT,
-      newText: text
-   }
-}
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 
 export default usersReducer;
