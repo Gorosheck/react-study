28dd0c13598df92707fa655278fe081cf7c7df02
Kirill Goroshko <gorosheck@gmail.com>
2023-05-05 21:46:50 +0300

    users-reducer done

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,5 +1,6 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
    users: [
@@ -14,9 +15,31 @@ const usersReducer = (state = initialState, action) => {
 
    switch (action.type) {
       case FOLLOW:
-
+         return {
+            ...state,
+            users: state.users.map(u => {
+               if (u.id === action.userId) {
+                  return { ...u, folowed: true }
+               }
+               return u;
+            })
+         }
 
       case UNFOLLOW:
+         return {
+            ...state,
+            users: state.users.map(u => {
+               if (u.id === action.userId) {
+                  return { ...u, folowed: false }
+               }
+               return u;
+            })
+         }
+
+      case SET_USERS: {
+         return { ...state, users: [...state.users, ...action.users] }
+      }
+
       default:
          return state;
    }
@@ -30,5 +53,6 @@ export const followAC = (userId) => {
 }
 
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
+export const setUsersAC = (users) => ({ type: SET_USERS, users })
 
 export default usersReducer;
