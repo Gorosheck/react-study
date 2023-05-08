66431f49f59de6d021e85f0f6e26eaf3e8009f13
Kirill Goroshko <gorosheck@gmail.com>
2023-05-08 17:01:32 +0300

    lesson 51 done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import styles from './Users.module.css'
 import axios from 'axios';
+import userPhoto from '../../ava-small.jpg';
 
 let Users = (props) => {
    if (props.users.length === 0) {
 
-      axios.get("https://social-network.samuraijs.com/api/1.0/users");
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+         props.setUsers(response.data.items)
+      });
 
-      props.setUsers([
-         { id: 1, photoUrl: 'https://img.freepik.com/premium-vector/cute-little-girl-avatar-round-profile-picture-isolated-on-white-background_176411-3067.jpg', followed: false, fullName: 'Sasha', status: '12', location: { city: 'Minsk', country: 'Belarus' } },
-         { id: 2, photoUrl: 'https://img.freepik.com/premium-vector/cute-little-girl-avatar-round-profile-picture-isolated-on-white-background_176411-3067.jpg', followed: true, fullName: 'Andrew', status: '11', location: { city: 'Moscow', country: 'Belarus' } },
-         { id: 3, photoUrl: 'https://img.freepik.com/premium-vector/cute-little-girl-avatar-round-profile-picture-isolated-on-white-background_176411-3067.jpg', followed: true, fullName: 'Alex', status: '11', location: { city: 'Kiev', country: 'Belarus' } },
-         { id: 4, photoUrl: 'https://img.freepik.com/premium-vector/cute-little-girl-avatar-round-profile-picture-isolated-on-white-background_176411-3067.jpg', followed: false, fullName: 'Vova', status: '11', location: { city: 'NY', country: 'Belarus' } }
-      ])
+
    }
+
+
    return <div>
       {
          props.users.map(u => <div key={u.id}>
             <span>
-               <div><img src={u.photoUrl} className={styles.userPhoto} /></div>
+               <div><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} /></div>
                <div>{u.followed
                   ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
                   : <button onClick={() => { props.follow(u.id) }}>follow</button>}</div>
             </span>
             <span>
                <span>
-                  <div>{u.fullName}</div>
+                  <div>{u.name}</div>
                   <div>{u.status}</div>
                </span>
                <span>
-                  <div>{u.location.city}</div>
-                  <div>{u.location.country}</div>
+                  <div>Minsk</div>
+                  <div>Belarus</div>
                </span>
             </span>
          </div>)
