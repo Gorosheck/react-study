c0f04594b25dd83394138e1d54ad0fbdbc778c38
Kirill Goroshko <gorosheck@gmail.com>
2023-05-11 23:33:43 +0300

    lesson 54 done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,8 @@ import axios from 'axios';
 import userPhoto from '../../ava-small.jpg';
 
 class Users extends React.Component {
-   constructor(props) {
-      super(props);
+
+   componentDidMount() {
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
          this.props.setUsers(response.data.items)
       });
@@ -13,7 +13,6 @@ class Users extends React.Component {
 
    render() {
       return <div>
-         <button onClick={this.getUsers}>Get users</button>
          {
             this.props.users.map(u => <div key={u.id}>
                <span>
