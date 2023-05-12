a6df7c289080d98e51d7dad05c2d3da1b87b1d9f
Kirill Goroshko <gorosheck@gmail.com>
2023-05-12 22:58:48 +0300

    lesson 55 - 10 min

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,11 @@ import axios from 'axios';
 import userPhoto from '../../ava-small.jpg';
 
 class Users extends React.Component {
-
    componentDidMount() {
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
          this.props.setUsers(response.data.items)
       });
    }
-
    render() {
       return <div>
          {
@@ -35,6 +33,5 @@ class Users extends React.Component {
          }
       </div >
    }
-
 }
 export default Users;
