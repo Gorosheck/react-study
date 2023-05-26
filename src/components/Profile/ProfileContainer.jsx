9c7f43585fcf26f4838107fbbe3cce9752c7207c
Kirill Goroshko <gorosheck@gmail.com>
2023-05-26 17:51:19 +0300

    60 - 24

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,8 +3,20 @@ import Profile from './Profile';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profile-reducer';
+import { withRouter } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+export function withRouter(Children) {
+   return (props) => {
+
+      const match = { params: useParams() };
+      return <Children {...props} match={match} />
+   }
+}
 
 class ProfileContainer extends React.Component {
+
+
 
    componentDidMount() {
       axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
@@ -23,7 +35,8 @@ let mapStateToProps = (state) => ({
    profile: state.profilePage.profile
 });
 
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
 export default connect(mapStateToProps, {
    setUserProfile
-})(ProfileContainer);
+})(WithUrlDataContainerComponent);
