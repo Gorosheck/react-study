56da07e048a5026a570c6ee9f5cadd732c145fa5
Kirill Goroshko <gorosheck@gmail.com>
2023-05-28 12:32:18 +0300

    lesson 61 - 20 min

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
    return (
       <header className={classes.header}>
          <img src='https://motoprokat.by/wp-content/uploads/2018/08/logo_bike.jpg' />
+         <div className={classes.loginBlock}>
+            <NavLink to={'/login'}>Login</NavLink>
+         </div>
       </header>
    );
 }
