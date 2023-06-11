891d31e87a21e81a143b50b77cfb8de48b8ac5c2
Kirill Goroshko <gorosheck@gmail.com>
2023-06-11 14:04:16 +0300

    lesson 69 - 10

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,21 +4,6 @@ import { connect } from 'react-redux';
 import { getUserProfile } from '../../redux/profile-reducer';
 import { useParams } from "react-router-dom";
 
-// function withRouter(Component) {
-//    function ComponentWithRouterProp(props) {
-//       let location = useLocation();
-//       let navigate = useNavigate();
-//       let params = useParams();
-//       return (
-//          <Component
-//             {...props}
-//             router={{ location, navigate, params }}
-//          />
-//       );
-//    }
-//    return ComponentWithRouterProp;
-// }
-
 export function withRouter(Children) {
    return (props) => {
 
