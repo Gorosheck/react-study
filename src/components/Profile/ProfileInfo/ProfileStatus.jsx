569c0fa892eb794a0bd500e993d37c998a8e67a9
Kirill Goroshko <gorosheck@gmail.com>
2023-06-15 16:43:30 +0300

    lesson 71 - 22

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 
-const ProfileStatus = (props) => {
-
-   return (
-      <div>
-
-      </div>
-   );
+class ProfileStatus extends React.Component {
+   state = {
+      editMode: true
+   }
+   render() {
+      return (
+         <div>
+            {!this.state.editMode &&
+               <div>
+                  <span>{this.props.status} sdvsvvsdcscsd</span>
+               </div>
+            }
+            {this.state.editMode &&
+               <div>
+                  <input value={this.props.status} />
+               </div>
+            }
+         </div >
+      );
+   }
 }
 export default ProfileStatus;
