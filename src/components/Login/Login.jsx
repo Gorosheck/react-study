7518da84ba2184f493a05be02de5e1b93312924f
Kirill Goroshko <gorosheck@gmail.com>
2023-06-25 21:36:43 +0300

    lesson 75 - 10

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 const Login = (props) => {
    return (
       <div>
-         Login
+         <h1>LOGIN</h1>
+         <form>
+            <div>
+               <input placeholder={"login"} />
+            </div>
+            <div>
+               <input placeholder={"pass"} />
+            </div>
+            <div>
+               <input type="checkbox" /> Remember me
+            </div>
+            <div>
+               <button>Submit</button>
+            </div>
+         </form>
       </div>
    );
 }
