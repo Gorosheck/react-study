72eac04b5b9c633b5757fcae5da3794b16b7dcf9
Kirill Goroshko <gorosheck@gmail.com>
2023-07-06 22:27:35 +0300

    lesson 77 - 27

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,7 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 import { Field, reduxForm } from 'redux-form';
 import { maxLengthCreator, required } from '../../../utils/validators/validators';
+import { Textarea } from '../../common/FormsControls/FormsControls';
 
 const maxLength10 = maxLengthCreator(10);
 
@@ -10,7 +11,7 @@ const maxLength10 = maxLengthCreator(10);
 const AddNewPostForm = (props) => {
    return (
       <form onSubmit={props.handleSubmit}>
-         <div><Field name="newPostText" component="textarea" validate={[required, maxLength10]} /></div>
+         <div><Field name="newPostText" component={Textarea} validate={[required, maxLength10]} placeholder='message' /></div>
          <div><button>Add post</button></div>
       </form>
    )
