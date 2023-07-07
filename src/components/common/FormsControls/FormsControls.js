277b29b968183f6ac35b842628749b3ddf9983b3
Kirill Goroshko <gorosheck@gmail.com>
2023-07-07 19:26:56 +0300

    lesson 77 - 36

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,9 +1,14 @@
 import React from "react"
+import styles from "./FormsControls.module.css"
 
 export const Textarea = ({ input, meta, ...props }) => {
+   const hasError = meta.touched && meta.error;
    return (
-      <div>
-         <textarea {...props} {...input} />
-      </div>
+      <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
+         <div>
+            <textarea {...props} {...input} />
+         </div>
+         {hasError && <span>{meta.error}</span>}
+      </div >
    )
 }
