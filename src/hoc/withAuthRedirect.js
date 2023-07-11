cacb53c75827315eee1e84318d3af00f2d8a45bf
Kirill Goroshko <gorosheck@gmail.com>
2023-07-11 17:41:46 +0300

    lesson 78 done

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -14,7 +14,6 @@ export const withAuthRedirect = (Component) => {
       }
    }
 
-
    let ConnectedAuthRedirectComponent = connect(mapStateToPropsRedirect)(RedirectComponent);
 
    return ConnectedAuthRedirectComponent;
