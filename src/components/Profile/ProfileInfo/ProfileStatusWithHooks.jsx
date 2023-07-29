b4ce13afb62506294dcd0957ed0a4b66d87d125a
Kirill Goroshko <gorosheck@gmail.com>
2023-07-29 20:48:16 +0300

    lesson 84 - 17 + theory

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProfileStatusWithHooks = (props) => {
+   let stateWithSetState = useState(false);
+   let editMode = stateWithSetState[0];
+   let setEditMode = stateWithSetState[1];
 
    return (
       <div>
