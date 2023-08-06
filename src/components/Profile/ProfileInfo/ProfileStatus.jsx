97bd7bc3151b0a47555b195941af7f715a78ab73
Kirill Goroshko <gorosheck@gmail.com>
2023-08-06 20:58:06 +0300

    lesson 87 - 9

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 
 class ProfileStatus extends React.Component {
-
    state = {
       editMode: false,
       status: this.props.status
    }
-
    activateEditMode = () => {
       this.setState({
          editMode: true
       });
    }
-
    deactivateEditMode = () => {
       this.setState({
          editMode: false
       });
       this.props.updateStatus(this.props.status);
    }
-
    onStatusChange = (e) => {
       this.setState({
          status: e.currentTarget.value
       });
    }
-
    componentDidUpdate = (prevProps, prevState) => {
       if (prevProps.status !== this.props.status) {
          this.setState({
@@ -33,7 +28,6 @@ class ProfileStatus extends React.Component {
          });
       }
    }
-
    render() {
       return (
          <div>
